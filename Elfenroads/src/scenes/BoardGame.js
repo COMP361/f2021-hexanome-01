8e83f88b0e46ca58d55affcbb0b92d84131a8beb
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2021-10-24 01:03:05 -0400

    update .gitignore to ignore package-lock.json (only used for ci, which we won't need in the project), also change the size of the game to be more dev friendly

--- Elfenroads/src/scenes/BoardGame.js
+++ Elfenroads/src/scenes/BoardGame.js
@@ -7,7 +7,7 @@ export default class BoardGame extends Phaser.Scene {
         // this is where all the images are imported
 
         this.load.image('brownBackground', 'assets/background/brown.jpg');
-        this.load.image('map', 'assets/boardgame/map.png')
+        this.load.image('map', 'assets/boardgame/map.png');
     }
 
     create() {
@@ -15,12 +15,12 @@ export default class BoardGame extends Phaser.Scene {
         // all the images are displayed here
 
         // setting the background
-        var background = this.add.image(0, 0, 'brownBackground').setOrigin(0,0);
+        let background = this.add.image(0, 0, 'brownBackground').setOrigin(0,0);
         background.displayWidth = this.sys.canvas.width;
         background.displayHeight = this.sys.canvas.height;
 
-        var map = this.add.image(400, 300, 'map');
-        map.setScale(0.1);
+        let map = this.add.image(1000, 500, 'map');
+        map.setScale(0.2);
 
     }
 }
