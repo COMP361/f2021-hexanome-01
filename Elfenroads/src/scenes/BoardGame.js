19fa31253dce1c0df78279a350a12fec31e9f6fc
Qiqi Su <calliasu2018@gmail.com>
2021-10-24 11:08:52 -0400

    Temporary remove changes related to move boot

--- Elfenroads/src/scenes/BoardGame.js
+++ Elfenroads/src/scenes/BoardGame.js
@@ -20,43 +20,10 @@ export default class BoardGame extends Phaser.Scene {
         background.displayWidth = this.sys.canvas.width;
         background.displayHeight = this.sys.canvas.height;
 
+    
         var map = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'map');
         map.setDisplaySize(this.cameras.main.width * 0.6, this.cameras.main.height * 0.7);
 
-        // move boot
-        var elfenboot = this.add.sprite(this.cameras.main.width *0.65, this.cameras.main.height * 0.55, 'boot').setInteractive();
-        elfenboot.setDisplaySize(this.cameras.main.width * 0.18, this.cameras.main.height * 0.18)
-        elfenboot.on('pointerover', function () {
-
-            this.setTint(0x00ff00);
-    
-        });
-    
-        elfenboot.on('pointerout', function () {
-    
-            this.clearTint();
-    
-        });
-
-        this.input.setDraggable(elfenboot);
-
-        this.input.on('dragstart', function (pointer, gameObject) {
-
-            gameObject.setTint(0x808080);
-
-        });
-
-        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-
-            gameObject.x = dragX;
-            gameObject.y = dragY;
-
-        });
-
-        this.input.on('dragend', function (pointer, gameObject) {
-
-            gameObject.clearTint();
-
-        });
+ 
     }
 }
