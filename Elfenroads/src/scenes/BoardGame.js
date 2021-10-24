699911a9f403f882645e57ea9e10d544e52e0bc9
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2021-10-24 03:02:30 -0400

    reformat

--- Elfenroads/src/scenes/BoardGame.js
+++ Elfenroads/src/scenes/BoardGame.js
@@ -20,7 +20,6 @@ export default class BoardGame extends Phaser.Scene {
         background.displayHeight = this.sys.canvas.height;
 
         const map = this.add.image(0, 0, 'map').setOrigin(-0.35, -0.1);
-
         map.setDisplaySize(0.6 * window.innerWidth, 0.75 * window.innerHeight);
 
     }
