c2f01d7ed2c6a93f3decf6fcdd43a4b3725c65df
Qiqi Su <calliasu2018@gmail.com>
2021-10-24 11:51:09 -0400

    Added draggable sprite boot

--- Elfenroads/src/scenes/BoardGame.js
+++ Elfenroads/src/scenes/BoardGame.js
@@ -20,10 +20,36 @@ export default class BoardGame extends Phaser.Scene {
         background.displayWidth = this.sys.canvas.width;
         background.displayHeight = this.sys.canvas.height;
 
-    
+        // set and resize map
         const map = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'map');
         map.setDisplaySize(this.cameras.main.width * 0.6, this.cameras.main.height * 0.7);
 
- 
+        /* move boot */
+        // set initial position and relative size
+        const elfenboot = this.add.sprite(this.cameras.main.width *0.65, this.cameras.main.height * 0.55, 'boot').setInteractive();
+        elfenboot.setDisplaySize(this.cameras.main.width * 0.15, this.cameras.main.height * 0.15)
+
+        // make elfenboot draggable to any position
+        this.input.setDraggable(elfenboot);
+
+        this.input.on('dragstart', function (pointer, gameObject) {
+
+            gameObject.setTint(0x808080);
+
+        });
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+
+        });
+
+        this.input.on('dragend', function (pointer, gameObject) {
+
+            gameObject.clearTint();
+
+        });
+    
     }
 }
