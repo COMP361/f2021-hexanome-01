5fa4f0bf19de45da00d503ac4d879fd32ae5eac1
Qiqi Su <calliasu2018@gmail.com>
2021-10-24 11:16:57 -0400

    Chang var into const

--- Elfenroads/src/scenes/BoardGame.js
+++ Elfenroads/src/scenes/BoardGame.js
@@ -16,12 +16,12 @@ export default class BoardGame extends Phaser.Scene {
         // all the images are displayed here
 
         // setting the background
-        var background = this.add.image(0, 0, 'brownBackground').setOrigin(0,0);
+        const background = this.add.image(0, 0, 'brownBackground').setOrigin(0,0);
         background.displayWidth = this.sys.canvas.width;
         background.displayHeight = this.sys.canvas.height;
 
     
-        var map = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'map');
+        const map = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'map');
         map.setDisplaySize(this.cameras.main.width * 0.6, this.cameras.main.height * 0.7);
 
  
