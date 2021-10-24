0661f7f6711433f07f5f0379d93d44ad64863775
Qiqi Su <calliasu2018@gmail.com>
2021-10-23 22:14:52 -0400

    Created boot sprite and made it draggable

--- Elfenroads/src/scenes/BoardGame.js
+++ Elfenroads/src/scenes/BoardGame.js
@@ -8,6 +8,7 @@ export default class BoardGame extends Phaser.Scene {
 
         this.load.image('brownBackground', 'assets/background/brown.jpg');
         this.load.image('map', 'assets/boardgame/map.png')
+        this.load.image('boot', 'assets/boardgame/green_boot.png')
     }
 
     create() {
@@ -19,8 +20,43 @@ export default class BoardGame extends Phaser.Scene {
         background.displayWidth = this.sys.canvas.width;
         background.displayHeight = this.sys.canvas.height;
 
-        var map = this.add.image(400, 300, 'map');
-        map.setScale(0.1);
+        var map = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'map');
+        map.setDisplaySize(this.cameras.main.width * 0.6, this.cameras.main.height * 0.7);
 
+        // move boot
+        var elfenboot = this.add.sprite(this.cameras.main.width *0.65, this.cameras.main.height * 0.55, 'boot').setInteractive();
+        elfenboot.setDisplaySize(this.cameras.main.width * 0.18, this.cameras.main.height * 0.18)
+        elfenboot.on('pointerover', function () {
+
+            this.setTint(0x00ff00);
+    
+        });
+    
+        elfenboot.on('pointerout', function () {
+    
+            this.clearTint();
+    
+        });
+
+        this.input.setDraggable(elfenboot);
+
+        this.input.on('dragstart', function (pointer, gameObject) {
+
+            gameObject.setTint(0x808080);
+
+        });
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+
+        });
+
+        this.input.on('dragend', function (pointer, gameObject) {
+
+            gameObject.clearTint();
+
+        });
     }
 }
