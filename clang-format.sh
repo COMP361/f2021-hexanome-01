09d0f02cb498b3386f71f3c4e196bee45ba82ae5
Junjie <junjie.yang2@mail.mcgill.ca>
2021-10-28 01:07:41 -0400

    fix a typo

--- clang-format.sh
+++ clang-format.sh
@@ -25,8 +25,8 @@ for FILE in Elfenroads/src/scenes/* \
             Elfenroads/src/components/MainMenu/Modal/Create/* \
             Elfenroads/src/components/MainMenu/Modal/Login/* \
             Elfenroads/src/components/MainMenu/Modal/Wait/* \
-            Elfenroads/src/components/MainMenu/Modal/*Welcome/*\ ;
-            Elfenroads/src/components/MainMenu/Scene/* \ do
+            Elfenroads/src/components/MainMenu/Modal/*Welcome/* \
+            Elfenroads/src/components/MainMenu/Scene/* do
   case $FILE in
     *.js|*.ts)
       if [ "$1" = "diff" ]
