9af4ecf0a7c66771127cb911ec9e4a3445514b09
Diego Dorantes-Ferreira <63019625+diegodofe@users.noreply.github.com>
2021-10-28 00:25:46 -0400

    Create toggle settings menu with toggle buttons (#13)

--- Elfenroads/src/components/Game/Game.js
+++ Elfenroads/src/components/Game/Game.js
@@ -1,35 +1,37 @@
 import './Game.scss';
+
+// React/ion-phaser stuff
+import {IonPhaser} from '@ion-phaser/react';
 import Phaser from 'phaser';
 
 // Plug in for better image scaling
-import {Plugin as NineSlicePlugin} from 'phaser3-nineslice'
+import {Plugin as NineSlicePlugin} from 'phaser3-nineslice';
+import {useRef, useState} from 'react';
 
-// All of our scenes in Phaser Game
-import Preloader from '../../scenes/Preloader'
-import MainScene from '../../scenes/MainScene'
-import UIScene from '../../scenes/UIScene'
-import BoardScene from '../../scenes/BoardScene'
+import BoardScene from '../../scenes/BoardScene';
+import MainScene from '../../scenes/MainScene';
 import MoveBootScene from '../../scenes/MoveBootScene';
 
-// React/ion-phaser stuff
-import { IonPhaser } from '@ion-phaser/react';
-import { useState, useRef } from 'react';
+// All of our scenes in Phaser Game
+import Preloader from '../../scenes/Preloader';
+import SettingsScene from '../../scenes/SettingsScene';
+import UIScene from '../../scenes/UIScene';
 
 // Phaser Game config
 const game = {
-    type: Phaser.AUTO,
-    width: "100%",
-    height: "100%",
-    plugins: {
-		global: [ NineSlicePlugin.DefaultCfg ]
-	},
-    scene: [Preloader, MainScene, UIScene, BoardScene, MoveBootScene]
+  type: Phaser.AUTO,
+  width: '100%',
+  height: '100%',
+  plugins: {global: [NineSlicePlugin.DefaultCfg]},
+  scene:
+      [Preloader, MainScene, BoardScene, UIScene, MoveBootScene, SettingsScene]
 }
 
-export default function Game () {
+export default function
+Game() {
     const gameRef = useRef(null)
-    const [initialize, setInitialize] = useState(true)
+    const [initialize] = useState(true)
     return (
-        <IonPhaser className="game" ref={gameRef} game={game} initialize={initialize} />
+        <IonPhaser className='game' ref={gameRef} game={game} initialize={initialize} />
     )
 }
