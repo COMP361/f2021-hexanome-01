b174c94be0c8b9d148f69a5b7bf089d79d6bbbc5
Qiqi Su <calliasu2018@gmail.com>
2021-10-28 15:05:09 -0400

    used clang-format on cmd

--- Elfenroads/src/scenes/MoveBootScene.js
+++ Elfenroads/src/scenes/MoveBootScene.js
@@ -7,51 +7,51 @@ export default class BoardGame extends Phaser.Scene {
 
   create() {
     /* for showing drag position */
-    // var label = this.add.text(0, 0, '', { font: "48px Arial Black", fill: "#c51b7d" });
-    // label.setStroke('#de77ae', 8);
+    // var label = this.add.text(0, 0, '', { font: "48px Arial Black", fill:
+    // "#c51b7d" }); label.setStroke('#de77ae', 8);
 
     /* hard code positions of towns */
-    // var elvenhold = 
+    // var elvenhold =
     this.add.zone(1040, 420, 80, 80).setRectangleDropZone(80, 80);
-    // var feodor = 
+    // var feodor =
     this.add.zone(840, 365, 60, 60).setRectangleDropZone(60, 60);
-    // var lapphalya = 
+    // var lapphalya =
     this.add.zone(830, 480, 60, 60).setRectangleDropZone(60, 60);
-    // var rivinia = 
+    // var rivinia =
     this.add.zone(990, 310, 60, 60).setRectangleDropZone(60, 60);
-    // var ergeren = 
+    // var ergeren =
     this.add.zone(1175, 310, 60, 60).setRectangleDropZone(60, 60);
-    // var beafa = 
+    // var beafa =
     this.add.zone(1200, 490, 60, 60).setRectangleDropZone(60, 60);
-    // var strykhaven = 
+    // var strykhaven =
     this.add.zone(1080, 540, 60, 60).setRectangleDropZone(60, 60);
-    // var virst = 
+    // var virst =
     this.add.zone(900, 570, 60, 60).setRectangleDropZone(60, 60);
-    // var jxara = 
+    // var jxara =
     this.add.zone(650, 560, 60, 60).setRectangleDropZone(60, 60);
-    // var mahdavikia = 
+    // var mahdavikia =
     this.add.zone(410, 570, 60, 60).setRectangleDropZone(60, 60);
-    // var grangor = 
+    // var grangor =
     this.add.zone(400, 450, 60, 60).setRectangleDropZone(60, 60);
-    // var kihrimah = 
+    // var kihrimah =
     this.add.zone(530, 400, 60, 60).setRectangleDropZone(60, 60);
-    // var dagamura = 
+    // var dagamura =
     this.add.zone(670, 435, 60, 60).setRectangleDropZone(60, 60);
-    // var albaran = 
+    // var albaran =
     this.add.zone(680, 340, 60, 60).setRectangleDropZone(60, 60);
-    // var parundia = 
+    // var parundia =
     this.add.zone(540, 285, 60, 60).setRectangleDropZone(60, 60);
-    // var usselen = 
+    // var usselen =
     this.add.zone(400, 225, 60, 60).setRectangleDropZone(60, 60);
-    // var wylhien = 
+    // var wylhien =
     this.add.zone(550, 150, 60, 60).setRectangleDropZone(60, 60);
-    // var jaccaranda = 
+    // var jaccaranda =
     this.add.zone(715, 190, 60, 60).setRectangleDropZone(60, 60);
-    // var throtmanni = 
+    // var throtmanni =
     this.add.zone(880, 255, 60, 60).setRectangleDropZone(60, 60);
-    // var tichih = 
+    // var tichih =
     this.add.zone(1060, 200, 60, 60).setRectangleDropZone(60, 60);
-    // var yttar = 
+    // var yttar =
     this.add.zone(380, 335, 60, 60).setRectangleDropZone(60, 60);
 
     /* move boot */
@@ -77,22 +77,19 @@ export default class BoardGame extends Phaser.Scene {
     });
 
     // if the boot is dragged to the drop zone, it will stay in it
-    this.input.on('drop', function (pointer, gameObject, dropZone) {
-
-        gameObject.x = dropZone.x;
-        gameObject.y = dropZone.y;
-
+    this.input.on('drop', function(pointer, gameObject, dropZone) {
+      gameObject.x = dropZone.x;
+      gameObject.y = dropZone.y;
     });
 
-    this.input.on('dragend', function (pointer, gameObject, dropped) {
-        gameObject.clearTint();
-        // label.setText(gameObject.x + ":" + gameObject.y);
-        if (!dropped) // otherwise the boot will be back to original position
-        {
-            gameObject.x = gameObject.input.dragStartX;
-            gameObject.y = gameObject.input.dragStartY;
-        }
+    this.input.on('dragend', function(pointer, gameObject, dropped) {
+      gameObject.clearTint();
+      // label.setText(gameObject.x + ":" + gameObject.y);
+      if (!dropped)  // otherwise the boot will be back to original position
+      {
+        gameObject.x = gameObject.input.dragStartX;
+        gameObject.y = gameObject.input.dragStartY;
+      }
     });
-
   }
 }
