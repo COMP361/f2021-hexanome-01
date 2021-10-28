372ee777ed51b57a9cf1950bb66521f2cf74ad20
Qiqi Su <63274929+KeikeiSo@users.noreply.github.com>
2021-10-28 15:44:51 -0400

    modified MoveBootScene.js so that boot can only move from town to town (#15)
    * modified so that boot can only move to another town
    
    * used clang-format on cmd
    
    * tried again for clang-format
    
    * deleted unused code

--- Elfenroads/src/scenes/MoveBootScene.js
+++ Elfenroads/src/scenes/MoveBootScene.js
@@ -6,10 +6,54 @@ export default class BoardGame extends Phaser.Scene {
   }
 
   create() {
+    /* hard code positions of towns */
+    // elvenhold
+    this.add.zone(1040, 420, 80, 80).setRectangleDropZone(80, 80);
+    // feodor
+    this.add.zone(840, 365, 60, 60).setRectangleDropZone(60, 60);
+    // lapphalya
+    this.add.zone(830, 480, 60, 60).setRectangleDropZone(60, 60);
+    // rivinia
+    this.add.zone(990, 310, 60, 60).setRectangleDropZone(60, 60);
+    // ergeren
+    this.add.zone(1175, 310, 60, 60).setRectangleDropZone(60, 60);
+    // beafa
+    this.add.zone(1200, 490, 60, 60).setRectangleDropZone(60, 60);
+    // strykhaven
+    this.add.zone(1080, 540, 60, 60).setRectangleDropZone(60, 60);
+    // virst
+    this.add.zone(900, 570, 60, 60).setRectangleDropZone(60, 60);
+    // jxara
+    this.add.zone(650, 560, 60, 60).setRectangleDropZone(60, 60);
+    // mahdavikia
+    this.add.zone(410, 570, 60, 60).setRectangleDropZone(60, 60);
+    // grangor
+    this.add.zone(400, 450, 60, 60).setRectangleDropZone(60, 60);
+    // kihrimah
+    this.add.zone(530, 400, 60, 60).setRectangleDropZone(60, 60);
+    // dagamura
+    this.add.zone(670, 435, 60, 60).setRectangleDropZone(60, 60);
+    // albaran
+    this.add.zone(680, 340, 60, 60).setRectangleDropZone(60, 60);
+    // parundia
+    this.add.zone(540, 285, 60, 60).setRectangleDropZone(60, 60);
+    // usselen
+    this.add.zone(400, 225, 60, 60).setRectangleDropZone(60, 60);
+    // wylhien
+    this.add.zone(550, 150, 60, 60).setRectangleDropZone(60, 60);
+    // jaccaranda
+    this.add.zone(715, 190, 60, 60).setRectangleDropZone(60, 60);
+    // throtmanni
+    this.add.zone(880, 255, 60, 60).setRectangleDropZone(60, 60);
+    // tichih
+    this.add.zone(1060, 200, 60, 60).setRectangleDropZone(60, 60);
+    // yttar
+    this.add.zone(380, 335, 60, 60).setRectangleDropZone(60, 60);
+
     /* move boot */
     // set initial position and relative size
     const elfenboot = this.add.sprite(this.cameras.main.width * 0.65, this.cameras.main.height * 0.55, 'boot').setInteractive();
-    elfenboot.setDisplaySize(this.cameras.main.width * 0.15, this.cameras.main.height * 0.15);
+    elfenboot.setDisplaySize(this.cameras.main.width * 0.04, this.cameras.main.height * 0.08);
 
     // make elfenboot draggable to any position
     this.input.setDraggable(elfenboot);
@@ -23,8 +67,18 @@ export default class BoardGame extends Phaser.Scene {
       gameObject.y = dragY;
     });
 
-    this.input.on('dragend', function(pointer, gameObject) {
+    // if the boot is dragged to the drop zone, it will stay in it
+    this.input.on('drop', function(pointer, gameObject, dropZone) {
+      gameObject.x = dropZone.x;
+      gameObject.y = dropZone.y;
+    });
+
+    this.input.on('dragend', function(pointer, gameObject, dropped) {
       gameObject.clearTint();
+      if (!dropped) {  // otherwise the boot will be back to original position
+        gameObject.x = gameObject.input.dragStartX;
+        gameObject.y = gameObject.input.dragStartY;
+      }
     });
   }
 }
