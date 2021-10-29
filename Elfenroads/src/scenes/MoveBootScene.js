20506542b06bb430b8306d98574275cc3230c4ec
tneustadter <90117565+tneustadter@users.noreply.github.com>
2021-10-29 03:37:18 -0400

    Added a whole bunch of stuff in sphagetti code; points tracker, piece… (#20)
    * Added a whole bunch of stuff in sphagetti code; points tracker, piece vis toggle, town class
    
    * fix formatting using clang-format, remove spot on elvenhold
    
    Co-authored-by: Junjie <junjie.yang2@mail.mcgill.ca>

--- Elfenroads/src/scenes/MoveBootScene.js
+++ Elfenroads/src/scenes/MoveBootScene.js
@@ -1,5 +1,31 @@
 import Phaser from 'phaser';
 
+import eventsCenter from './EventsCenter';
+import {Town} from './Towns';
+
+export const Towns = {
+  elvenhold: new Town('elvenhold', [1050, 400]),
+  feodor: new Town('feodor', [835, 370]),
+  lapphalya: new Town('lapphalya', [840, 470]),
+  rivinia: new Town('rivinia', [1005, 315]),
+  ergeren: new Town('ergeren', [1190, 315]),
+  beafa: new Town('beafa', [1200, 490]),
+  strykhaven: new Town('strykhaven', [1090, 540]),
+  virst: new Town('virst', [915, 570]),
+  jxara: new Town('jxara', [655, 560]),
+  mahdavikia: new Town('mahdavikia', [450, 555]),
+  grangor: new Town('grangor', [415, 455]),
+  kihrimah: new Town('kihrimah', [550, 410]),
+  dagamura: new Town('dagamura', [680, 435]),
+  albaran: new Town('albaran', [685, 335]),
+  parundia: new Town('parundia', [555, 285]),
+  usselen: new Town('usselen', [400, 225]),
+  wylhien: new Town('wylhien', [560, 160]),
+  jaccaranda: new Town('jaccaranda', [725, 185]),
+  throtmanni: new Town('throtmanni', [890, 250]),
+  tichih: new Town('tichih:', [1060, 200]),
+  yttar: new Town('yttar', [390, 330]),
+};
 export default class BoardGame extends Phaser.Scene {
   constructor() {
     super('movebootscene');
@@ -8,26 +34,50 @@ export default class BoardGame extends Phaser.Scene {
     const graphics = this.add.graphics();
     const zoneWidth = 60 / 1600 * this.cameras.main.width;
     const zoneHeight = 60 / 750 * this.cameras.main.height;
-    // town coordinates on a 1600 x 750 screen
-    // elvenhold, feodor, lapphalya, rivinia, ergeren, beafa, strykhaven, virst, jxara, mahdavikia, grangor, kihrimah, dagamura, albaran, parundia,
-    // usselen, wylhien, jaccaranda, throtmanni, tichih, yttar
-    const townsCoor = [
-      [1050, 400], [835, 370], [840, 470], [1005, 315], [1190, 315], [1200, 490], [1090, 540], [915, 570], [655, 560],  [450, 555], [415, 455],
-      [550, 410],  [680, 435], [685, 335], [555, 285],  [400, 225],  [560, 160],  [725, 185],  [890, 250], [1060, 200], [390, 330]
-    ];
-    // create dropzone for each town
-    for (let i = 0; i < townsCoor.length; i++) {
-      this.add.zone(townsCoor[i][0] / 1600 * this.cameras.main.width, townsCoor[i][1] / 750 * this.cameras.main.height, zoneWidth, zoneHeight)
-          .setRectangleDropZone(zoneWidth, zoneHeight);
+
+    // will have to have another class to define the player object in the future
+    const Player = {
+      name: 'green',
+      currentTown: Towns.elvenhold,
+      points: 0,
+    };
+
+    Towns.elvenhold.setCurrentPlayers({Player});
+
+    // Initialize each town
+    for (const town in Towns) {
+      if (Towns[town].position) {
+        this.add
+            .zone(
+                Towns[town].position[0] / 1600 * this.cameras.main.width, Towns[town].position[1] / 750 * this.cameras.main.height, zoneWidth,
+                zoneHeight)
+            .setRectangleDropZone(zoneWidth, zoneHeight);
+        if (Towns[town].name === 'elvenhold') {
+          continue;
+        }
+        Towns[town].setTownPieceHolder(this.add.circle(
+            Towns[town].position[0] / 1600 * this.cameras.main.width, (Towns[town].position[1] - 40) / 750 * this.cameras.main.height, 15, 0x000000));
+        Towns[town].setTownPieces(this.add.circle(
+            Towns[town].position[0] / 1600 * this.cameras.main.width, (Towns[town].position[1] - 40) / 750 * this.cameras.main.height, 10, 0x007700));
+      }
     }
+    this.updateVis();
+
+    // update visibility of town pieces
+    eventsCenter.on('update-town-piece-vis', this.updateVis, this);
 
     /* move boot */
+    const elvenboot =
+        this.add
+            .sprite(
+                Towns.elvenhold.position[0] / 1600 * this.cameras.main.width, Towns.elvenhold.position[1] / 750 * this.cameras.main.height, 'boot')
+            .setInteractive();
+
     // set initial position and relative size
-    const elfenboot = this.add.sprite(1050 / 1600 * this.cameras.main.width, 400 / 750 * this.cameras.main.height, 'boot').setInteractive();
-    elfenboot.setDisplaySize(this.cameras.main.width * 0.04, this.cameras.main.height * 0.08);
+    elvenboot.setDisplaySize(this.cameras.main.width * 0.04, this.cameras.main.height * 0.08);
 
     // make elfenboot draggable to any position
-    this.input.setDraggable(elfenboot);
+    this.input.setDraggable(elvenboot);
 
     this.input.on('dragstart', function(pointer, gameObject) {
       gameObject.setTint(0x808080);
@@ -38,14 +88,27 @@ export default class BoardGame extends Phaser.Scene {
       gameObject.y = dragY;
       graphics.lineStyle(2, 0xffff00, 1);
       // highlight every draggable town
-      for (let i = 0; i < townsCoor.length; i++) {
-        graphics.strokeRect(
-            (townsCoor[i][0] - 30) / 1600 * this.cameras.main.width, (townsCoor[i][1] - 30) / 750 * this.cameras.main.height, zoneWidth, zoneHeight);
+      for (const town in Towns) {
+        if (Towns[town].position) {
+          graphics.strokeRect(
+              (Towns[town].position[0] - 30) / 1600 * this.cameras.main.width, (Towns[town].position[1] - 30) / 750 * this.cameras.main.height,
+              zoneWidth, zoneHeight);
+        }
       }
     });
 
     // if the boot is dragged to the drop zone, it will stay in it
     this.input.on('drop', function(pointer, gameObject, dropZone) {
+      for (const town in Towns) {
+        if (Towns[town].position[0] / 1600 * this.cameras.main.width === dropZone.x &&
+            Towns[town].position[1] / 750 * this.cameras.main.height === dropZone.y) {
+          if (Towns[town].townPieces.active) {
+            Towns[town].townPieces.destroy();
+            Player.points++;
+            eventsCenter.emit('update-points', Player.points);
+          }
+        }
+      }
       gameObject.x = dropZone.x;
       gameObject.y = dropZone.y;
     });
@@ -60,5 +123,18 @@ export default class BoardGame extends Phaser.Scene {
       // clear towns highlight
       graphics.clear();
     });
+  }
+
+  updateVis() {
+    for (const town in Towns) {
+      if (Towns[town]) {
+        if (Towns[town].townPieces.active) {
+          Towns[town].townPieces.setVisible(!Towns[town].townPieces.visible);
+        }
+        if (Towns[town].townPieceHolder.active) {
+          Towns[town].townPieceHolder.setVisible(!Towns[town].townPieceHolder.visible);
+        }
+      }
+    }
   }
 }
--- Elfenroads/src/scenes/UIScene.js
+++ Elfenroads/src/scenes/UIScene.js
@@ -1,5 +1,7 @@
 import Phaser from 'phaser';
 
+import eventsCenter from './EventsCenter';
+
 export default class UIScene extends Phaser.Scene {
   constructor() {
     super('uiscene');
@@ -10,9 +12,19 @@ export default class UIScene extends Phaser.Scene {
     this.scene.launch('movebootscene');
     this.scene.launch('settingsscene');
 
+    // Show our dude
+    const p = this.add.sprite(this.cameras.main.width / 7, this.cameras.main.height / 6, 'green-actor');
+    this.points = this.add.text(this.cameras.main.width / 7.3, this.cameras.main.height / 6.4, '0', {
+      fontSize: 32,
+      color: '#000000',
+    });
+    p.setScale(2);
+
+    eventsCenter.on('update-points', this.updatePoints, this);
+
     /* show other player token */
-    const boot = this.add.sprite(this.cameras.main.width / 7, this.cameras.main.height / 5, 'blue-actor');
-    const bootCards = this.add.sprite(this.cameras.main.width / 7 + boot.displayWidth * .47, this.cameras.main.height / 5, 'blue-actorcards');
+    const boot = this.add.sprite(this.cameras.main.width / 7, this.cameras.main.height / 4, 'blue-actor');
+    const bootCards = this.add.sprite(this.cameras.main.width / 7 + boot.displayWidth * .47, this.cameras.main.height / 4, 'blue-actorcards');
 
     boot.setScale(0.3);
     bootCards.setScale(0.3);
@@ -50,5 +62,27 @@ export default class UIScene extends Phaser.Scene {
         .on('pointerup', function() {
           cheatSheet.setVisible(!cheatSheet.visible);
         });
+
+    /* toggles town piece visibility */
+    const townPieceButton = this.add.sprite(width - 130, 30, 'grey-box');
+    this.add.image(townPieceButton.x, townPieceButton.y, 'information').setScale(0.7);
+
+    // Add interactivity
+    townPieceButton.setInteractive()
+        .on('pointerdown',
+            function() {
+              this.setTint(0xd3d3d3);
+            })
+        .on('pointerout',
+            function() {
+              this.clearTint();
+            })
+        .on('pointerup', function() {
+          eventsCenter.emit('update-town-piece-vis', true);
+        });
+  }
+
+  updatePoints(points) {
+    this.points.text = `${points}`;
   }
 }
