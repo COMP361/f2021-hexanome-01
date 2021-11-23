2620b3d0a0f582273acdf5dac36e8d0e455b14dc
Junjie <junjie.yang2@mail.mcgill.ca>
2021-11-23 18:58:38 -0500

    fix db issue

--- backend/README.md
+++
@@ -1,4 +0,0 @@
-Refer this blog for development.
-https://blog.logrocket.com/how-to-build-a-graphql-api-with-nestjs/
-Lobby Service Port:
-4242
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import DatabaseConfig from './config/database.config';
-import { ConnectionOptions } from 'typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(DatabaseConfig() as ConnectionOptions),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "elfenroads.westus3.cloudapp.azure.com",
+      port: 5432,
+      username: "comp361",
+      password: "elfenroad",
+      database: "elfenroad",
+      entities: [
+        "dist/**/*.model.js"
+      ],
+      synchronize: false
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql'
     }),
