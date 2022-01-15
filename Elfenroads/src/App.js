20c4d8a8da74502d9c9021a9b61f7939ccba11fb
Qiqi Su <calliasu2018@gmail.com>
2022-01-15 15:20:20 -0500

    Revert "modify player token scene"
    This reverts commit fabdf110fc8b55e0b8fb05b620f41a700efa7392.

--- Elfenroads/src/App.js
+++ Elfenroads/src/App.js
@@ -1,14 +1,14 @@
 import './App.scss';
-import MainMenu from './components/MainMenu';
+// import MainMenu from './components/MainMenu';
 import Game from './components/Game/Game';
-import {useState} from 'react';
+// import {useState} from 'react';
 
 function App() {
-  const [game, setGame] = useState(null);
+  // const [game, setGame] = useState(null);
 
   return (
     <div className="App">
-      {game ? <Game /> : <MainMenu setGame={setGame} />}
+      <Game />
     </div>
   );
 }
--- Elfenroads/src/scenes/PlayerTokenScene.ts
+++ Elfenroads/src/scenes/PlayerTokenScene.ts
@@ -2,12 +2,17 @@ import Phaser from 'phaser';
 import PlayerToken from '../classes/PlayerToken';
 
 export default class PlayerTokenScene extends Phaser.Scene {
+  xOrigin: number;
+  yOrigin: number;
+
   constructor() {
     super('playertokenscene');
+    this.xOrigin = this.cameras.main.width / 7;
+    this.yOrigin = this.cameras.main.height / 4;
   }
 
   create() {
-    const player1 = new PlayerToken(this, this.cameras.main.width / 7, this.cameras.main.height / 4, 'blue-actor');
+    const player1 = new PlayerToken(this, this.xOrigin, this.yOrigin, 'blue-actor');
     player1.addCounter('unknown-counter');
     player1.addCounter('pig-counter');
     player1.addCounter('cloud-counter');
