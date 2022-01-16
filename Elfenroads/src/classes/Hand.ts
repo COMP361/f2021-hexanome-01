eaad2ea8c06dd9469eca865e2b9cc1d141cf5d42
Diego Ferreira <diego.dorantes-ferreira@mail.mcgill.ca>
2022-01-16 13:55:02 -0500

    Fix map for Hand and CardScene

--- Elfenroads/src/classes/Hand.ts
+++ Elfenroads/src/classes/Hand.ts
@@ -5,57 +5,55 @@ export default class Hand {
   container: Phaser.GameObjects.Container
   isOpen: boolean;
   numCards: number;
-  store: Map<string, string>;
+  store: Map<string, string> = new Map([
+    ['dragonCard', 'dragon-card'],
+    ['giantPigCard', 'giant-pig-card'],
+    ['elfCycleCard', 'elf-cycle-card'],
+    ['magicCloudCard', 'magic-cloud-card'],
+    ['uicornCard', 'unicorn-card'],
+    ['trollWagonCard', 'troll-wagon-card'],
+    ['raftCard', 'raft-card'],
+    ['witchCard', 'witch-card'],
+    ['goldCard', 'gold-card'],
+  ]);
 
   constructor(scene: Phaser.Scene) {
     this.scene = scene;
     this.numCards = 0;
-    
+
     // Define all variables required
     const {height} = this.scene.scale;
     const {width} = this.scene.scale;
     const CARD_UP = height / 1.12;
-
 
     // Initialize container to group elements
     this.container = scene.add.container(width / 4.2, CARD_UP);
 
     // Initialize/make settings menu hidden
     this.isOpen = true;
-
-    // Create All Card Objects
-    const store = new Map();
-    store.set('dragonCard', 'dragon-card');
-    store.set('giantPigCard', 'giant-pig-card');
-    store.set('elfCycleCard', 'elf-cycle-card');
-    store.set('magicCloudCard', 'magic-cloud-card');
-    store.set('uicornCard', 'unicorn-card');
-    store.set('trollWagonCard', 'troll-wagon-card');
-    store.set('raftCard', 'raft-card');
-    store.set('witchCard', 'witch-card');
-    store.set('goldCard', 'gold-card');
-    this.store = store;
   }
 
-  addCard(cardName: string){
+  addCard(cardName: string) {
     const CARD_SIZE = 0.2;
     const img = this.store.get(cardName);
-    const card = this.scene.add.sprite(this.numCards*30, 0, img!);
-    card
-        .setData({
-          name: cardName,
-        })
-        .setScale(CARD_SIZE);
-    this.container.add(card);
-    this.numCards ++;
+
+    if (img !== undefined) {
+      const card = this.scene.add.sprite(this.numCards * 30, 0, img);
+      card.setData({
+            name: cardName,
+          })
+          .setScale(CARD_SIZE);
+      this.container.add(card);
+      this.numCards++;
+    }
   }
 
-  removeCard(cardName: string){
-    let children = this.scene.children;
-    let child = children.getByName(cardName);
+  removeCard(cardName: string) {
+    const children = this.scene.children;
+    const child = children.getByName(cardName);
     if (child !== null) {
       child.destroy(true);
-      this.numCards --;
+      this.numCards--;
     }
   }
 
