57164ac22e4f42189aef8d02934ae9edae60a46f
Qiqi Su <calliasu2018@gmail.com>
2022-01-16 01:11:25 -0500

    Revert "refactor card scene"
    This reverts commit 8ea886a5edc7bc3c42cc3149fc09db7bdd52d55e.

--- Elfenroads/src/classes/Hand.ts
+++ Elfenroads/src/classes/Hand.ts
@@ -4,18 +4,13 @@ export default class Hand {
   scene: Phaser.Scene;
   container: Phaser.GameObjects.Container
   isOpen: boolean;
-  numCards: number;
-  store: Map<string, string>;
 
   constructor(scene: Phaser.Scene) {
     this.scene = scene;
-    this.numCards = 0;
-    
-    // Define all variables required
     const {height} = this.scene.scale;
     const {width} = this.scene.scale;
     const CARD_UP = height / 1.12;
-
+    const CARD_SIZE = 0.2;
 
     // Initialize container to group elements
     this.container = scene.add.container(width / 4.2, CARD_UP);
@@ -24,40 +19,80 @@ export default class Hand {
     this.isOpen = true;
 
     // Create All Card Objects
-    const store = new Map();
-    store.set('dragonCard', 'dragon-card');
-    store.set('giantPigCard', 'giant-pig-card');
-    store.set('elfCycleCard', 'elf-cycle-card');
-    store.set('magicCloudCard', 'magic-cloud-card');
-    store.set('uicornCard', 'unicorn-card');
-    store.set('trollWagonCard', 'troll-wagon-card');
-    store.set('raftCard', 'raft-card');
-    store.set('witchCard', 'witch-card');
-    store.set('goldCard', 'gold-card');
-    this.store = store;
-  }
-
-  addCard(cardName: string){
-    const CARD_SIZE = 0.2;
-    const img = this.store.get(cardName);
-    const card = this.scene.add.sprite(this.numCards*30, 0, img!);
-    card
+    const dragonCard = this.scene.add.sprite(0, 0, 'dragon-card');
+    dragonCard
         .setData({
-          name: cardName,
+          name: 'dragonCard',
         })
         .setScale(CARD_SIZE);
-    this.container.add(card);
-    this.numCards ++;
+    this.container.add(dragonCard);
+
+    const giantPigCard = this.scene.add.sprite(30, 0, 'giant-pig-card');
+    giantPigCard
+        .setData({
+          name: 'giantPigCard',
+        })
+        .setScale(CARD_SIZE);
+    this.container.add(giantPigCard);
+
+    const elfCycleCard = this.scene.add.sprite(60, 0, 'elf-cycle-card');
+    elfCycleCard
+        .setData({
+          name: 'elfCycleCard',
+        })
+        .setScale(CARD_SIZE);
+    this.container.add(elfCycleCard);
+
+
+    const magicCloudCard = this.scene.add.sprite(90, 0, 'magic-cloud-card');
+    magicCloudCard
+        .setData({
+          name: 'magicCloudCard',
+        })
+        .setScale(CARD_SIZE);
+    this.container.add(magicCloudCard);
+
+    const unicornCard = this.scene.add.sprite(120, 0, 'unicorn-card');
+    unicornCard
+        .setData({
+          name: 'unicornCard',
+        })
+        .setScale(CARD_SIZE);
+    this.container.add(unicornCard);
+
+    const trollWagonCard = this.scene.add.sprite(150, 0, 'troll-wagon-card');
+    trollWagonCard
+        .setData({
+          name: 'trollWagonCard',
+        })
+        .setScale(CARD_SIZE);
+    this.container.add(trollWagonCard);
+
+    const raftCard = this.scene.add.sprite(180, 0, 'raft-card');
+    raftCard
+        .setData({
+          name: 'raftCard',
+        })
+        .setScale(CARD_SIZE);
+    this.container.add(raftCard);
+
+    const witchCard = this.scene.add.sprite(210, 0, 'witch-card');
+    witchCard
+        .setData({
+          name: 'witchCard',
+        })
+        .setScale(CARD_SIZE);
+    this.container.add(witchCard);
+
+    const goldCard = this.scene.add.sprite(240, 0, 'gold-card');
+    goldCard
+        .setData({
+          name: 'goldCard',
+        })
+        .setScale(CARD_SIZE);
+    this.container.add(goldCard);
   }
 
-  removeCard(cardName: string){
-    let children = this.scene.children;
-    let child = children.getByName(cardName);
-    if (child !== null) {
-      child.destroy(true);
-      this.numCards --;
-    }
-  }
 
   // Show entire settings menu
   show() {
