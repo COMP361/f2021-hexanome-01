0c169e024719dfaeee9e260250077f10cd2bcbf8
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2022-03-05 19:17:28 -0500

    add versions (#56)
    * add versions
    
    * add sockets
    
    * enhance socket
    
    * modify port

--- backend/src/GameService.ts
+++ backend/src/GameService.ts
@@ -6,7 +6,76 @@ export default class GameService {
     baseURL: 'http://elfenroads.westus3.cloudapp.azure.com:4242/',
   });
 
-  private async getOAuthToken(): Promise<void> {
+  async createLSUser(
+    name: string,
+    password: string,
+    preferredColour: string,
+    role: string,
+  ): Promise<string> {
+    let access_token = '';
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic YmdwLWNsaWVudC1uYW1lOmJncC1jbGllbnQtcHc=',
+      },
+    };
+
+    await this.instance
+      .post(
+        'oauth/token?grant_type=password&username=maex&password=abc123_ABC123',
+        {},
+        config,
+      )
+      .then((response) => {
+        access_token = response.data['access_token'];
+      });
+
+    return await this.instance
+      .put(
+        encodeURI(`api/users/${name}?access_token=${access_token}`).replace(
+          /\+/g,
+          '%2B',
+        ),
+        {
+          name: name,
+          password: password,
+          preferredColour: preferredColour,
+          role: role,
+        },
+      )
+      .then((response) => {
+        return response.data as string;
+      })
+      .catch((error) => {
+        return error.response['data'] as string;
+      });
+  }
+
+  private async createLSUsers(): Promise<void> {
+    await this.createLSUser(
+      'ElfenlandVer1',
+      'f2021-EFLDV1',
+      'FFFFFF',
+      'ROLE_SERVICE',
+    ).catch(() => {
+      return;
+    });
+
+    await this.createLSUser(
+      'ElfenlandVer2',
+      'f2021-EFLDV2',
+      'FFFFFF',
+      'ROLE_SERVICE',
+    ).catch(() => {
+      return;
+    });
+  }
+
+  private async getOAuthToken(
+    username: string,
+    password: string,
+  ): Promise<void> {
     const config = {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -16,7 +85,7 @@ export default class GameService {
     await this.instance
       .post(
         encodeURI(
-          'oauth/token?grant_type=password&username=Elfenroad&password=f2021-EFRD',
+          `oauth/token?grant_type=password&username=${username}&password=${password}`,
         ).replace(/\+/g, '%2B'),
         {},
         config,
@@ -29,28 +98,32 @@ export default class GameService {
       });
   }
 
-  async registerGameService(): Promise<void> {
+  private async registerGameService(
+    service: string,
+    username: string,
+    password: string,
+  ): Promise<void> {
     await this.instance
-      .get(encodeURI('api/gameservices/Elfenroad'))
+      .get(encodeURI(`api/gameservices/${service}`))
       .then(() => {
         return;
       })
       .catch(async () => {
         const data = {
-          location: 'http://elfenroads.westus3.cloudapp.azure.com:3454/',
+          location: 'http://elfenroads.westus3.cloudapp.azure.com:3455/',
           maxSessionPlayers: 6,
           minSessionPlayers: 2,
-          name: 'Elfenroad',
-          displayName: 'Elfenroad',
-          webSupport: 'true',
+          name: service,
+          displayName: service,
+          webSupport: 'false',
         };
 
-        await this.getOAuthToken();
+        await this.getOAuthToken(username, password);
 
         await this.instance
           .put(
             encodeURI(
-              `api/gameservices/Elfenroad?access_token=${this.token}`,
+              `api/gameservices/${service}?access_token=${this.token}`,
             ).replace(/\+/g, '%2B'),
             data,
             {
@@ -63,5 +136,20 @@ export default class GameService {
             console.log(error);
           });
       });
+  }
+
+  async registerGameServices(): Promise<void> {
+    await this.createLSUsers();
+    await this.registerGameService(
+      'ElfenlandVer1',
+      'ElfenlandVer1',
+      'f2021-EFLDV1',
+    );
+
+    await this.registerGameService(
+      'ElfenlandVer2',
+      'ElfenlandVer2',
+      'f2021-EFLDV2',
+    );
   }
 }
