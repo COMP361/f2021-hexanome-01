ee2afef904a88f3eebfc18f194684542b0e50c72
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2022-03-06 15:13:45 -0500

    Add more players and make new icon function void

--- Elfenroads/src/scenes/PlayerIconScene.ts
+++ Elfenroads/src/scenes/PlayerIconScene.ts
@@ -1,5 +1,4 @@
 import Phaser from 'phaser';
-import {BootColour} from '../enums/BootColour';
 import PlayerIcon from '../classes/PlayerIcon';
 import PlayerManager from '../managers/PlayerManager';
 import Player from '../classes/Player';
@@ -9,39 +8,29 @@ export default class PlayerTokenScene extends Phaser.Scene {
     super('playericonscene');
   }
 
-  private addAllPlayer(): Array<PlayerIcon> {
+  private createPlayerIcons(): void {
     const icons: Array<PlayerIcon> = [];
     const players: Array<Player> = PlayerManager.getInstance().getPlayers();
+
     for (let i = 0; i < players.length; i++) {
       icons.push(
         new PlayerIcon(
           this,
           this.cameras.main.width / 7,
           this.cameras.main.height / 4 + 70 * i,
-          BootColour.Green
+          players[i].getBootColour()
         )
       );
     }
-    return icons;
   }
 
   create() {
-    const player1: PlayerIcon = new PlayerIcon(
-      this,
-      this.cameras.main.width / 7,
-      this.cameras.main.height / 4,
-      BootColour.Green
-    );
-    player1.addCounter('unknown-counter');
-    player1.addCounter('pig-counter');
-    player1.addCounter('cloud-counter');
+    this.createPlayerIcons();
 
-    const player2: PlayerIcon = new PlayerIcon(
-      this,
-      this.cameras.main.width / 7,
-      this.cameras.main.height / 4 + 70,
-      BootColour.Blue
-    );
-    player2.addCounter('unknown-counter');
+    //   player1.addCounter('unknown-counter');
+    //   player1.addCounter('pig-counter');
+    //   player1.addCounter('cloud-counter');
+
+    //   player2.addCounter('unknown-counter');
   }
 }
