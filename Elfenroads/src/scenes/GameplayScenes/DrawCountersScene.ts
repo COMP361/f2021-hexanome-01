011dff7bce062a3787a34149a87b4a1b0c3e174e
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2022-03-09 01:57:37 -0500

    Render new counter on draw

--- Elfenroads/src/scenes/GameplayScenes/DrawCountersScene.ts
+++ Elfenroads/src/scenes/GameplayScenes/DrawCountersScene.ts
@@ -3,7 +3,7 @@ import ItemManager from '../../managers/ItemManager';
 import PlayerManager from '../../managers/PlayerManager';
 
 export default class DrawCountersScene extends Phaser.Scene {
-  // Global array to store buttons in this scene
+  // Grab width of current game to center our container
 
   constructor() {
     super('drawcountersscene');
@@ -21,13 +21,11 @@ export default class DrawCountersScene extends Phaser.Scene {
       }
     );
 
+    const gameWidth: number = this.cameras.main.width;
     // Create brown ui panel element relative to the size of the text
     const brownPanel: Phaser.GameObjects.RenderTexture = this.add
       .nineslice(0, 0, drawCounterText.width + 20, 40, 'brown-panel', 24)
       .setOrigin(0, 0);
-
-    // Grab width of current game to center our container
-    const gameWidth: number = this.cameras.main.width;
 
     // Initialize container to group elements
     // Need to center the container relative to the gameWidth and the size of the text box
@@ -40,34 +38,34 @@ export default class DrawCountersScene extends Phaser.Scene {
     container.add(brownPanel);
     container.add(drawCounterText);
 
-    let previousWidth: integer = 0;
+    let previousWidth: integer = gameWidth / 2 + 150;
     for (let i = 0; i < 4; i++) {
-      let currentItem = ItemManager.getInstance().getRandomItem();
-
       previousWidth += 50;
-
-      const itemSprite = this.add
-        .sprite(gameWidth / 2 + 150 + previousWidth, 110, currentItem.getName())
-        .setScale(0.25)
-        .setInteractive()
-        .on('pointerdown', () => {
-          itemSprite.setTint(0xd3d3d3);
-        })
-        .on('pointerout', () => {
-          itemSprite.clearTint();
-        })
-        .on('pointerup', () => {
-          itemSprite.clearTint();
-          PlayerManager.getInstance().getCurrentPlayer().addItem(currentItem);
-          currentItem = ItemManager.getInstance().getRandomItem();
-          itemSprite.destroy();
-          this.scene.stop('inventoryscene');
-          this.scene.launch('inventoryscene');
-          this.scene.stop('playericonscene');
-          this.scene.launch('playericonscene');
-        });
+      this.generateCounter(previousWidth);
     }
   }
 
-  generateCounter(): void {}
+  generateCounter(previousWidth: integer): void {
+    const currentItem = ItemManager.getInstance().getRandomItem();
+    const itemSprite = this.add
+      .sprite(previousWidth, 110, currentItem.getName())
+      .setScale(0.25)
+      .setInteractive()
+      .on('pointerdown', () => {
+        itemSprite.setTint(0xd3d3d3);
+      })
+      .on('pointerout', () => {
+        itemSprite.clearTint();
+      })
+      .on('pointerup', () => {
+        itemSprite.clearTint();
+        PlayerManager.getInstance().getCurrentPlayer().addItem(currentItem);
+        itemSprite.destroy();
+        this.generateCounter(previousWidth);
+        this.scene.stop('inventoryscene');
+        this.scene.launch('inventoryscene');
+        this.scene.stop('playericonscene');
+        this.scene.launch('playericonscene');
+      });
+  }
 }
