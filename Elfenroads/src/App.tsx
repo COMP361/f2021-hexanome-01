ed643184e5478acac9df6c417e5466f081fec5ef
Qiqi Su <calliasu2018@gmail.com>
2022-03-09 14:06:42 -0500

    revert changes to App.tsx

--- Elfenroads/src/App.tsx
+++ Elfenroads/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.scss';
+import MainMenu from './components/MainMenu';
 import Game from './components/Game/Game';
+import {useState} from 'react';
 import React from 'react';
 
 export default function App() {
+  const [socket, setSocket] = useState(null);
+
   return (
     <div className="App">
-      <Game />
+      {socket ? <Game socket={socket} /> : <MainMenu setSocket={setSocket} />}
     </div>
   );
 }
