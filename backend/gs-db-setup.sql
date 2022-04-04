6cdf9d4a34ced248cbb945d0c0da42fa7b97c939
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2022-04-03 22:04:06 -0400

    remove files

--- backend/gs-db-setup.sql
+++
@@ -1,5 +0,0 @@
-USE mysql;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
-CREATE USER 'comp361'@'%' IDENTIFIED BY 'comp3612021';
-CREATE DATABASE cs361;
-GRANT ALL PRIVILEGES ON comp361.* TO 'comp361'@'%';
